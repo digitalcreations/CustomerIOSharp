db7d883b9f3839651687624241586e5b5cecb2a7
Vegard Andreas Larsen <vegard@digitalcreations.no>
2022-08-26 14:32:30 +0200

    Use record.

--- CustomerIOSharp/CustomerDetails.cs
+++ CustomerIOSharp/CustomerDetails.cs
@@ -1,14 +1,3 @@
 ﻿namespace CustomerIOSharp;
 
-public class CustomerDetails : ICustomerDetails
-{
-    public CustomerDetails(string id, string email)
-    {
-        Id = id;
-        Email = email;
-    }
-
-    public string Id { get; }
-
-    public string Email { get; }
-}
+public record CustomerDetails(string Id, string Email) : ICustomerDetails;
